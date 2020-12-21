bbb04d351cbb6b1cd1850186d4a1922627b7b3ea
lorenanda <lorena.ciutacu@gmail.com>
2020-12-21 11:48:49 +0100

    add sentiment analysis function

--- app/models.py
+++ app/models.py
@@ -1,18 +1,28 @@
 import spacy
-from spacy import displacy
 from collections import Counter
 import de_core_news_sm
+from textblob_de import TextBlobDE
 
 nlp_de = de_core_news_sm.load()
+
+def preprocess_text(txt):
+    f = open(txt, "r")
+    f_read = f.read()
+    text = nlp_de(f_read)
+    return text
+
+def analyse_sentiment(text):
+    text_polarity = round(TextBlobDE(str(text)).sentiment.polarity, 3)
+    text_subjectivity = round(TextBlobDE(str(satz)).sentiment.subjectivity, 3)
+    print("Text Sentiment:", text_polarity)
+    print("Text Subjektivität:", text_subjectivity)
 
 def summarize_text(text):
     pass
 
-def extract_entities(txt):
-    f = open(txt, "r")
-    f_read = f.read()
-    text = nlp_de(f_read)
-    
+
+
+def extract_entities(text):
     entities_nr = len(text.ents)
     print(entities_nr, "Entities in diesem Text.")
 
@@ -25,5 +35,8 @@ def extract_entities(txt):
     entities_list = [(X.text, X.label_) for X in text.ents]
     print("Entities im Text:\n", entities_list)
 
-def analyse_sentiment(text):
-    pass
+
+if __name__ == "__main__":
+    input_text = preprocess_text("app/texts/example1.txt")
+    analyse_sentiment(input_text)
+    extract_entities(input_text)
