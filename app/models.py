19833200fabfad10bccbd578370b0168b45be83d
lorenanda <lorena.ciutacu@gmail.com>
2020-12-21 11:02:47 +0100

    add function to extract entities

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 def summarize_text(text):
     pass
 
-def extract_entities(text):
-    pass
+def extract_entities(txt):
+    f = open(txt, "r")
+    f_read = f.read()
+    text = nlp_de(f_read)
+    
+    entities_list = [(X.text, X.label_) for X in text.ents]
+    return entities_list
 
 def analyse_sentiment(text):
     pass
